836e207e7a55d5bf9d885efb3ee44df96fcfcf7f
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2024-01-19 16:51:14 +0530

    contact with emailjs

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import React, { useRef,useState } from 'react';
+import emailjs from '@emailjs/browser';
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Switch } from '@headlessui/react'
 import Navbar from '@/Components/Navbar'
@@ -7,6 +8,62 @@ function classNames(...classes) {
 }
 
 const ContactUs = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    number:"",
+    message: "",
+  });
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        'service',
+        'template_d1a0buq',
+        {
+          from_name: form.name,
+          to_name: "Kaaikani",
+          from_email: form.email,
+          to_email: "kaaikanionline@gmail.com",
+          from_number: form.number,
+          message: form.message,
+        },
+        'oY9bifZAvL_QPid2n'
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            number:"",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
+  };
+
   return (
     <div>
       <div className="isolate  bg-gray-900 text-white px-6 lg:px-8">
@@ -31,36 +88,25 @@ const ContactUs = () => {
           Aute magna irure deserunt veniam aliqua magna enim voluptate.
         </p>
       </div>
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+      <form action="#"   ref={formRef}
+          onSubmit={handleSubmit} method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-          <div>
+          <div  className="sm:col-span-2">
             <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-300/70">
-              First name
+             Your  Name
             </label>
             <div className="mt-2.5">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm shadow-indigo-300 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+            <input
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your good name?"
+              className='block w-full rounded-md border-0 px-3.5 py-2  text-gray-900 shadow-sm  shadow-indigo-300  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+            />
             </div>
           </div>
-          <div>
-            <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-300/70">
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autoComplete="family-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm  shadow-indigo-300  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+          
           
           <div className="sm:col-span-2">
             <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-300/70">
@@ -71,6 +117,9 @@ const ContactUs = () => {
                 type="email"
                 name="email"
                 id="email"
+                value={form.email}
+                onChange={handleChange}
+                placeholder="What's your web address?"
                 autoComplete="email"
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm  shadow-indigo-300  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -84,8 +133,11 @@ const ContactUs = () => {
               
               <input
                 type="tel"
-                name="phone-number"
-                id="phone-number"
+                name="number"
+                id="number"
+                value={form.number}
+                onChange={handleChange}
+                placeholder="What's your number?"
                 autoComplete="tel"
                 className="block w-full rounded-md border-0 px-3.5 py-2  text-gray-900 shadow-sm  shadow-indigo-300  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -99,7 +151,10 @@ const ContactUs = () => {
               <textarea
                 name="message"
                 id="message"
+                value={form.message}
+              onChange={handleChange}
                 rows={4}
+                placeholder='How may I assist you?'
                 className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm  shadow-indigo-300  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 defaultValue={''}
               />
@@ -110,9 +165,10 @@ const ContactUs = () => {
         <div className="mt-10">
           <button
             type="submit"
+            value="Send"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Let's talk
+            {loading ? "Sending..." : "Let's Talk"}
           </button>
         </div>
       </form>

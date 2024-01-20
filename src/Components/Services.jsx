c189db4cf2278cc50725a00c4c0aa23cc893aa24
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2024-01-20 22:09:14 +0530

    ui changed-2

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -15,8 +15,8 @@ const Services = () => {
               <div className="h-px flex-auto bg-gray-700" />
             </div>
           </div>
-          <div class="mt-10 shadow-xl grid divide-x divide-y divide-gray-100 dark:divide-gray-700 overflow-hidden rounded-3xl border border-gray-100 dark:border-gray-700 sm:grid-cols-2 lg:grid-cols-4 lg:divide-y-0 xl:grid-cols-4">
-            <div class="group relative bg-white dark:bg-gray-600 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+          <div class="mt-10 shadow-xl grid divide-x divide-y divide-gray-200 dark:divide-gray-700 overflow-hidden rounded-3xl border border-gray-100 dark:border-gray-700 sm:grid-cols-2 lg:grid-cols-4 lg:divide-y-0 xl:grid-cols-4">
+            <div class="group relative bg-gradient-to-br from-black to-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
               <div class="relative space-y-8 py-12 p-8">
                 <img
                   src="/Services/trolley.png"
@@ -38,7 +38,7 @@ const Services = () => {
                 </div>
               </div>
             </div>
-            <div class="group relative bg-white dark:bg-gray-700 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+            <div class="group relative bg-gradient-to-br from-black to-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
               <div class="relative space-y-8 py-12 p-8">
                 <img
                   src="/Services/fast-delivery.png"
@@ -60,7 +60,7 @@ const Services = () => {
                 </div>
               </div>
             </div>
-            <div class="group relative bg-white dark:bg-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+            <div class="group relative bg-gradient-to-br from-black to-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
               <div class="relative space-y-8 py-12 p-8">
                 <img
                   src="/Services/login.png"
@@ -83,7 +83,7 @@ const Services = () => {
                 </div>
               </div>
             </div>
-            <div class="group relative bg-gray-50 dark:bg-gray-900 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
+            <div class="group relative bg-gradient-to-br from-black to-gray-800 transition hover:z-[1] hover:shadow-2xl hover:shadow-gray-600/10">
               <div class="relative space-y-8 py-12 p-8 transition duration-300 group-hover:bg-white dark:group-hover:bg-gray-800">
                 <img
                   src="/Services/sustainability.png"
